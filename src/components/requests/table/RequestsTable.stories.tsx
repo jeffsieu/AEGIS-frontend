63d3a428918739a4c051466be09d9bcd253a6636
ryanntannn <tanryan777@gmail.com>
2022-05-26 10:43:08 +0800

    add RequestsTable

---
+++ src/components/requests/table/RequestsTable.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentStory } from '@storybook/react';
+import { AvailableQualifiedMember } from '@types';
+import { MOCK_QUALIFIED_MEMBERS } from '@utils/mock-data/schedule';
+import dayjs from 'dayjs';
+import RequestsTable from './RequestsTable';
+
+export default {
+  title: 'Requests/Requests Table',
+  component: RequestsTable,
+  argTypes: {},
+};
+
+const Template: ComponentStory<typeof RequestsTable> = (args) => (
+  <RequestsTable {...args}/>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+	requests: [
+		{callsign: "Upin", dates: [[dayjs('2022-05-01'),dayjs('2022-05-01')], [dayjs('2022-05-02'), dayjs('2022-05-05')]], reason: "Chao geng"},
+		{callsign: "Ipin", dates: [[dayjs('2022-05-01'),dayjs('2022-05-01')], [dayjs('2022-05-02'), dayjs('2022-05-05')]], reason: "Go back to village, approved by OC"}
+	]
+};
