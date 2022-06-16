e8b4b703e8698017f965a1b24034fb02faca1e49
Jeff Sieu <jeffsy00@gmail.com>
2022-06-16 19:40:05 +0800

    Add blurred edges to scrolled schedule

--- src/components/schedule/FullWidthScheduleContainer/FullWidthScheduleContainer.tsx
+++ src/components/schedule/FullWidthScheduleContainer/FullWidthScheduleContainer.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
 import { PropsWithChildren } from 'react';
 import './index.css';
 
@@ -7,11 +7,43 @@ export type FullWidthScheduleContainerProps = {};
 function FullWidthScheduleContainer(
   props: PropsWithChildren<FullWidthScheduleContainerProps>
 ) {
+  const theme = useTheme();
+
   return (
-    <Box width="100vw" position="relative" className="full-width-container">
-      <Box display="flex">
-        <Box paddingX={4}>{props.children}</Box>
+    <Box width="100vw" position="relative" display="inline-block">
+      <Box
+        className="full-width-container"
+        sx={{
+          '&::before': {
+            pointerEvents: 'none',
+            width: '100%',
+            height: '100%',
+            content: '""',
+            // background: 'red',
+            position: 'absolute',
+            zIndex: 5,
+            background: `linear-gradient(-90deg, ${theme.palette.background.paper} 0px, ${theme.palette.background.paper} 16px, transparent 32px);`,
+          },
+          '&:hover': {
+            '&::before': {
+              height: 'calc(100% - 32px)',
+            },
+          },
+        }}
+      >
+        <Box display="flex">
+          <Box paddingLeft={2} paddingRight={4} paddingBottom={2}>
+            {props.children}
+          </Box>
+        </Box>
       </Box>
+      {/* <Box
+        width="100%"
+        height="100%"
+        zIndex={2}
+        position="absolute"
+        top={0}
+      ></Box> */}
     </Box>
   );
 }
