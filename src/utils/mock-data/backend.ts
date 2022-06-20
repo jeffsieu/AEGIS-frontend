d5f54e7c1260360b15856cd834e66505e839deba
gjlam95 <gj_lam@hotmail.com>
2022-06-20 09:51:58 +0800

    update mock data

--- src/utils/mock-data/backend.ts
+++ src/utils/mock-data/backend.ts
@@ -4,31 +4,121 @@ import dayjs from 'dayjs';
 
 export const MEMBERS: Backend.Member[] = [
   {
-    callsign: 'Nano',
-    squadron: '18',
-    type: 'ADMIN',
+    callsign: 'Tim',
+    squadron: '3',
+    type: 'MEMBER',
   },
   {
-    callsign: 'Crafter',
+    callsign: 'Volley',
+    squadron: '3',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Gambit',
+    squadron: '3',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Trickshot',
+    squadron: '3',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Muru',
+    squadron: '69',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Sean',
+    squadron: '69',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Lennon',
+    squadron: '69',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Tofu',
     squadron: '18',
     type: 'MEMBER',
   },
   {
-    callsign: 'Ram',
+    callsign: 'Egal',
     squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Focus',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Seed',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Crafter',
+    squadron: '18',
+    type: 'ADMIN',
+  },
+  {
+    callsign: 'Rudolph',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Karma',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Sonic',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Kym',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Threemen',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Halftime',
+    squadron: '18',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Bentoh',
+    squadron: 'HQ',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Slayer',
+    squadron: 'HQ',
+    type: 'MEMBER',
+  },
+  {
+    callsign: 'Faisal',
+    squadron: 'HQ',
     type: 'MEMBER',
   },
 ];
 
 export const ROLES: Backend.Role[] = [
   {
-    name: 'A2',
+    name: 'G4 COMD',
   },
   {
     name: 'G4',
   },
   {
-    name: 'G4 COMD',
+    name: 'A2',
   },
 ];
 
@@ -40,15 +130,87 @@ export type MockQualification = {
 export const QUALIFICATIONS: MockQualification[] = [
   {
     memberId: 1,
-    roles: ['A2', 'G4'],
+    roles: ['G4 COMD', 'G4'],
   },
   {
     memberId: 2,
-    roles: ['G4', 'G4 COMD'],
+    roles: ['G4 COMD', 'G4'],
   },
   {
     memberId: 3,
-    roles: ['A2', 'G4 COMD'],
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 4,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 5,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 6,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 7,
+    roles: ['G4', 'A2'],
+  },
+  {
+    memberId: 8,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 9,
+    roles: ['G4'],
+  },
+  {
+    memberId: 10,
+    roles: ['G4', 'A2'],
+  },
+  {
+    memberId: 11,
+    roles: ['G4'],
+  },
+  {
+    memberId: 12,
+    roles: ['G4', 'A2'],
+  },
+  {
+    memberId: 13,
+    roles: ['G4'],
+  },
+  {
+    memberId: 14,
+    roles: ['G4'],
+  },
+  {
+    memberId: 15,
+    roles: ['G4'],
+  },
+  {
+    memberId: 16,
+    roles: ['G4', 'A2'],
+  },
+  {
+    memberId: 17,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 18,
+    roles: ['G4'],
+  },
+  {
+    memberId: 19,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 20,
+    roles: ['G4 COMD', 'G4'],
+  },
+  {
+    memberId: 21,
+    roles: ['G4 COMD', 'G4'],
   },
 ];
 
@@ -62,17 +224,17 @@ export const SCHEDULES: Backend.Schedule[] = [
       ].flatMap((date, index) => [
         {
           roleId: 1,
-          memberId: index % 2 === 0 ? 1 : 2,
+          memberId: Math.floor(Math.random() * 22),
           date: dayjs(date).format('YYYY-MM-DD'),
         },
         {
           roleId: 2,
-          memberId: index % 2 === 0 ? 1 : 3,
+          memberId: Math.floor(Math.random() * 22),
           date: dayjs(date).format('YYYY-MM-DD'),
         },
         {
           roleId: 3,
-          memberId: index % 2 === 0 ? 2 : 3,
+          memberId: Math.floor(Math.random() * 22),
           date: dayjs(date).format('YYYY-MM-DD'),
         },
       ]),
