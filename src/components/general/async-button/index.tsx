773a907d31b4c8cbe6f6aaabc9b56e7fd7d53281
Jeff Sieu <jeffsy00@gmail.com>
2022-06-22 17:41:21 +0800

    Fix AsyncButton passing invalid props to LoadingButton

--- src/components/general/async-button/index.tsx
+++ src/components/general/async-button/index.tsx
@@ -1,48 +1,64 @@
-import { LoadingButton, LoadingButtonProps } from "@mui/lab";
-import { Alert, Snackbar } from "@mui/material";
-import { string } from "prop-types";
-import React from "react";
+import { LoadingButton, LoadingButtonProps } from '@mui/lab';
+import { Alert, Snackbar } from '@mui/material';
+import { string } from 'prop-types';
+import { useState } from 'react';
 
 type AsyncButtonProps = {
-	asyncRequest: () => void;
-}
+  asyncRequest: () => Promise<void>;
+};
 
-export function AsyncButton(props: LoadingButtonProps & AsyncButtonProps){
-	const [isLoading, setIsLoading] = React.useState(false);
-	const [isSnackbarOpen, setIsSnackbarOpen] = React.useState(false);
-	const [snackbarMessage, setSnackbarMessage] = React.useState('');
+export function AsyncButton(props: LoadingButtonProps & AsyncButtonProps) {
+  const { asyncRequest, ...loadingButtonProps } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
 
-	function getErrorString(err: any): string{
-		if(err instanceof string) return (err as string);
-		if(err.status != undefined && err.data.errors.length > 0 && err.data.errors[0].message) return err.data.errors[0].message;
-		return "Error";
-	}
+  function getErrorString(err: any): string {
+    if (err instanceof string) return err as string;
+    if (
+      err.status !== undefined &&
+      err.data.errors.length > 0 &&
+      err.data.errors[0].message
+    )
+      return err.data.errors[0].message;
+    return 'Error';
+  }
 
-	async function onClick(){
-		try{
-			setIsLoading(true);
-			await props.asyncRequest();
-			setIsLoading(false);
-		} catch(err: any) {
-			setIsLoading(false);
-			setSnackbarMessage(getErrorString(err));
-			setIsSnackbarOpen(true);
-		}
-	}
+  async function onClick() {
+    try {
+      setIsLoading(true);
+      await asyncRequest();
+      setIsLoading(false);
+    } catch (err: any) {
+      setIsLoading(false);
+      setSnackbarMessage(getErrorString(err));
+      setIsSnackbarOpen(true);
+    }
+  }
 
-	function handleClose(){
-		setIsSnackbarOpen(false);
-	}
+  function handleClose() {
+    setIsSnackbarOpen(false);
+  }
 
-	return <>
-		<LoadingButton loading={isLoading} {...props} onClick={onClick}>
-			{props.children}
-		</LoadingButton>
-		
-		<Snackbar open={isSnackbarOpen} autoHideDuration={6000} onClose={handleClose}>
-				<Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-					{snackbarMessage}
-				</Alert>
-		</Snackbar>
-	</>
+  return (
+    <>
+      <LoadingButton
+        loading={isLoading}
+        onClick={onClick}
+        {...loadingButtonProps}
+      >
+        {props.children}
+      </LoadingButton>
+
+      <Snackbar
+        open={isSnackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </>
+  );
 }
