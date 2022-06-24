35fafe4dca0a08219d0a151fdc27e8e8afdd8610
Jeff Sieu <jeffsy00@gmail.com>
2022-06-24 13:00:19 +0800

    AEGIS-111 Revamp PlannerHomePage

--- src/views/Member/MemberHomePage/MemberHomePage.tsx
+++ src/views/Member/MemberHomePage/MemberHomePage.tsx
@@ -1,65 +1,12 @@
-import EmptyHint from '@components/general/empty-hint';
 import TitledContainer from '@components/general/titled-container';
-import FullWidthScheduleContainer from '@components/schedule/FullWidthScheduleContainer/FullWidthScheduleContainer';
-import ScheduleTable from '@components/schedule/ScheduleTable/ScheduleTable';
-import { Box, Typography } from '@mui/material';
-import {
-  useGetMembersQuery,
-  useGetRolesQuery,
-  useGetScheduleForMonthQuery,
-} from '@services/backend';
-import { ERROR_SCHEDULE_NOT_READY } from '@utils/constants/string';
-import { useBuildWithApiQueries } from '@utils/helpers/api-builder';
-import { scheduleToScheduleTableProps } from '@utils/helpers/schedule';
-import dayjs from 'dayjs';
+import ThisMonthSchedule from '@views/Shared/HomePage/ThisMonthSchedule';
 
 function MemberHomePage() {
-  return useBuildWithApiQueries({
-    queries: {
-      schedule: useGetScheduleForMonthQuery({
-        month: dayjs().format('YYYY-MM'),
-        isPublished: true,
-      }),
-      roles: useGetRolesQuery(),
-      members: useGetMembersQuery(),
-    },
-    onSuccess: ({ schedule, roles, members }) => {
-      const getScheduleComponent = () => {
-        if (schedule === null) {
-          return <EmptyHint>{ERROR_SCHEDULE_NOT_READY}</EmptyHint>;
-        }
-
-        const props = scheduleToScheduleTableProps(schedule, roles, members);
-        return (
-          <Box
-            display="flex"
-            position="relative"
-            flexDirection="column"
-            alignItems="center"
-          >
-            <FullWidthScheduleContainer>
-              <Box paddingLeft="calc(50vw - 600px + 24px - 36px)">
-                <ScheduleTable
-                  {...props}
-                  stickyHeader={true}
-                  canFilter={true}
-                />
-              </Box>
-            </FullWidthScheduleContainer>
-          </Box>
-        );
-      };
-
-      return (
-        <TitledContainer title="Home">
-          <Typography variant="h5" gutterBottom>
-            This month
-          </Typography>
-          {getScheduleComponent()}
-        </TitledContainer>
-      );
-    },
-  });
+  return (
+    <TitledContainer title="Home">
+      <ThisMonthSchedule showDraft={false} />
+    </TitledContainer>
+  );
 }
 
 export default MemberHomePage;
