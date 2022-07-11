a55d09406c11a39f7c8a28fd18334e9f944ef78d
Jeff Sieu <jeffsy00@gmail.com>
2022-07-11 18:24:16 +0800

    Add optimistic request deletion

--- src/services/backend/requests.ts
+++ src/services/backend/requests.ts
@@ -47,6 +47,24 @@ export const requestsApi = baseApi.injectEndpoints({
         body: requestIds,
       }),
       invalidatesTags: [{ type: 'Requests', id: 'LIST' }],
+      onQueryStarted(requestIds, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          requestsApi.util.updateQueryData(
+            'getRequests',
+            undefined,
+            (requests: RequestResponse[]) => {
+              // Mutate list
+              requestIds.forEach((id) => {
+                const index = requests.findIndex((r) => r.id === id);
+                if (index !== -1) {
+                  requests.splice(index, 1);
+                }
+              });
+            }
+          )
+        );
+        queryFulfilled.catch(patchResult.undo);
+      },
     }),
     updateRequest: builder.mutation<void, Backend.WithId<Backend.Request>>({
       query: (request) => ({
