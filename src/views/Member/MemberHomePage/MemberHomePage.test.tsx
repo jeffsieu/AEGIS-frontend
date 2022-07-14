ad7e1d26d4d76c3bed8989a88c2b9b25c8233ef3
ryanntannn <tanryan777@gmail.com>
2022-07-14 16:16:12 +0800

    refractor test to be compliant to BDD style

--- src/views/Member/MemberHomePage/MemberHomePage.test.tsx
+++ src/views/Member/MemberHomePage/MemberHomePage.test.tsx
@@ -12,31 +12,30 @@ beforeEach((): void => {
   fetchMock.doMock();
 });
 
-it('should render the latest month schedule', async () => {
-  const { result } = renderHook(() => MemberHomePage());
-  expect(result.current.props.children.type.name).toBe('ThisMonthSchedule');
-});
+describe('when page loads', () => {
+  it('should render the latest month schedule', async () => {
+    const mockSchedule = createMockScheduleItems(
+      new Date('2022-07-01'),
+      new Date('2022-07-30'),
+      ROLES
+    );
 
-test('working schedule component', async () => {
-  const mockSchedule = createMockScheduleItems(
-    new Date('2022-07-01'),
-    new Date('2022-07-30'),
-    ROLES
-  );
+    fetchMock.mockResponse((req) => {
+      switch (req.url) {
+        case `${BACKEND_URL}/roleInstances`:
+          return new Promise((res) => res(JSON.stringify(ROLES)));
+        case `${BACKEND_URL}/members?includeRoles=true`:
+          return new Promise((res) => res(JSON.stringify(MEMBERS)));
+        default:
+          return new Promise((res) =>
+            res(JSON.stringify({ month: '2022-07', duties: mockSchedule }))
+          );
+      }
+    });
 
-  fetchMock.mockResponse((req) => {
-    switch (req.url) {
-      case `${BACKEND_URL}/roleInstances`:
-        return new Promise((res) => res(JSON.stringify(ROLES)));
-      case `${BACKEND_URL}/members?includeRoles=true`:
-        return new Promise((res) => res(JSON.stringify(MEMBERS)));
-      default:
-        return new Promise((res) =>
-          res(JSON.stringify({ month: '2022-07', duties: mockSchedule }))
-        );
-    }
+    renderWithProviders(<ThisMonthSchedule showDraft={true} />);
+    await waitFor(() =>
+      expect(screen.getByText('Jul 2022')).toBeInTheDocument()
+    );
   });
-
-  renderWithProviders(<ThisMonthSchedule showDraft={true} />);
-  await waitFor(() => expect(screen.getByText('Jul 2022')).toBeInTheDocument());
 });
